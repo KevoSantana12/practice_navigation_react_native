b0e380c5b01fa0d024cb78290ef7386900b044b4
Kevin Santana <klinarte258@gmail.com>
2025-10-09 19:38:41 -0600

    fonts and colors added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { useFonts } from 'expo-font';
 
 import './global.css';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'work-black': require('./assets/fonts/WorkSans-Black.ttf'),
+    'work-light': require('./assets/fonts/WorkSans-Light.ttf'),
+    'work-medium': require('./assets/fonts/WorkSans-Medium.ttf'),
+
+  });
+
+  if (!fontsLoaded) {
+    return null; // o un loading spinner
+  }
+
   return (
     <>
-      <View className="flex-1 items-center justify-center bg-white">
-        <Text className="text-xl font-bold text-blue-500">
+      <View className="flex-1 items-center justify-center bg-primary" >
+        <Text className="text-xl text-secondary-100" style={{ fontSize: 20, fontFamily: 'work-medium' }}>
           Welcome to Nativewind porraaa!
         </Text>
       </View>
