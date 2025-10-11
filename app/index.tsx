4a30557b6c870ac610d6e07ca62f8b6ae7b30eba
Kevin Santana <klinarte258@gmail.com>
2025-10-10 22:51:32 -0600

    Expo actualizado y router agregado

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+
+
+import { Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-lg font-bold text-blue-600">
+                Bienvenido a mi app Expo Router + Tailwind 🎉
+            </Text>
+        </View>
+    );
+}
