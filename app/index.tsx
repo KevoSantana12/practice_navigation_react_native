e88de6cd41bd558b7fe4fed3a58e1e5809636859
Kevin Santana <klinarte258@gmail.com>
2025-10-18 14:32:19 -0600

    routing agregado

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,20 @@
 
 
+import { Link, Redirect } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
     return (
-        <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-lg font-bold text-blue-600">
-                Bienvenido a mi app Expo Router + Tailwind 🎉
-            </Text>
-        </View>
+        // <View className="flex-1 items-center justify-center bg-white">
+        //     <Text className="text-lg font-bold text-red-600">
+        //         Bienvenido a mi app Expo Router + Tailwind porrra 🎉
+        //     </Text>
+
+        //     <Link href="/products">Products</Link>
+        // </View>
+
+        <Redirect href='/home'>
+
+        </Redirect>
     );
 }
---
+++ app/products/index.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+
+const ProductsScreeen = () => {
+    return (
+        <View>
+            <Text>
+                Products Screen
+            </Text>
+            <Text className="text-lg font-bold text-red-600">
+                Bienvenido a mi app Expo Router + Tailwind 🎉
+            </Text>
+
+        </View>
+    );
+};
+
+export default ProductsScreeen;
